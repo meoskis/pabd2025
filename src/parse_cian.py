302b6b9519e6cca6f22b3923cfc52d9ffeacc402
Nikita Kudryashov <nkmeowski@gmail.com>
2025-06-15 22:00:33 +0300

    fix

--- src/parse_cian.py
+++ src/parse_cian.py
@@ -1,40 +1,38 @@
-"""  Parse data from cian.ru
-https://github.com/lenarsaitov/cianparser
-"""
+import os
 import datetime
-
-import cianparser
 import pandas as pd
+import cianparser
+from utils import setup_logger
 
-moscow_parser = cianparser.CianParser(location="Москва")
+logger = setup_logger()
+
+RAW_DIR = "data/raw"
 
 
-def main():
-    """
-    Function docstring
-    """
+def parse_cian():
+    moscow_parser = cianparser.CianParser(location="Москва")
     t = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-    n_rooms = 1
-    
+
     data = []
     for n_rooms in range(1, 4):
-        
         new_data = moscow_parser.get_flats(
             deal_type="sale",
             rooms=(n_rooms,),
             with_saving_csv=False,
             additional_settings={
-                "start_page": 1,
-                "end_page": 5,
-                "object_type": "secondary"
-            })
+                "start_page": 7,
+                "end_page": 9,
+                "object_type": "secondary",
+            },
+        )
         data.extend(new_data)
-        
+
     df = pd.DataFrame(data)
+    os.makedirs(RAW_DIR, exist_ok=True)
+    csv_path = f"{RAW_DIR}/flats_{t}.csv"
+    df.to_csv(csv_path, encoding="utf-8", index=False)
+    logger.info(f"Сохранены данные в {csv_path}")
 
-    csv_path = f'data/raw/1-3_rooms_{t}.csv'
-    df.to_csv(csv_path, encoding='utf-8', index=False)
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    parse_cian()
